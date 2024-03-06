f612ddb47fe497ae3a8a53f64613995a020fb10c
kuldeep <kuldeep.appic@gmail.com>
2024-03-06 21:27:33 +0530

    ui design

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,HeaderComponent],
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss']
 })
