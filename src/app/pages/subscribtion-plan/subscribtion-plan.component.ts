b4884aaef01fa6ad572b4b82b865ff5f14a97979
kuldeep <kuldeep.appic@gmail.com>
2024-03-18 16:53:59 +0530

    module 1 mobile responsive

--- src/app/pages/subscribtion-plan/subscribtion-plan.component.ts
+++ src/app/pages/subscribtion-plan/subscribtion-plan.component.ts
@@ -5,17 +5,85 @@ import { MatCardModule } from '@angular/material/card';
 import { HeaderComponent } from 'src/app/shared/header/header.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Router } from '@angular/router';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-subscribtion-plan',
   standalone: true,
-  imports: [CommonModule,MatButtonModule, MatCardModule,MatTabsModule],
+  imports: [CommonModule,MatButtonModule, MatCardModule,MatTabsModule,CarouselModule],
   templateUrl: './subscribtion-plan.component.html',
   styleUrls: ['./subscribtion-plan.component.scss']
 })
 export class SubscribtionPlanComponent {
   isActive = false;
 
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: false,
+    touchDrag: true,
+    dots: true,
+    autoplay: true,
+    navSpeed: 300,
+    nav: false,
+    margin: 15,
+
+    autoplayTimeout: 8000,
+    autoplaySpeed: 1500,
+    // navText: ["", ""],
+    // navText: ["<img class='navTxtImg' src='./assets/images/icons/left-chevron-svgrepo-com.svg'>", "<img class='navTxtImg' src='./assets/images/icons/left-chevron-svgrepo-com.svg'>"],
+    responsive: {
+      0: {
+        items: 1,
+        skip_validateItems: true,
+      },
+      400: {
+        items: 1,
+        skip_validateItems: true,
+      },
+      740: {
+        items: 2,
+        skip_validateItems: true,
+      },
+      940: {
+        items: 3,
+        skip_validateItems: true,
+      },
+    },
+  };
+
+  customOptions1: OwlOptions = {
+    loop: false,
+    mouseDrag: false,
+    touchDrag: true,
+    dots: true,
+    autoplay: true,
+    navSpeed: 300,
+    nav: false,
+    margin: 15,
+
+    autoplayTimeout: 8000,
+    autoplaySpeed: 1500,
+    // navText: ["", ""],
+    // navText: ["<img class='navTxtImg' src='./assets/images/icons/left-chevron-svgrepo-com.svg'>", "<img class='navTxtImg' src='./assets/images/icons/left-chevron-svgrepo-com.svg'>"],
+    responsive: {
+      0: {
+        items: 1,
+        skip_validateItems: true,
+      },
+      400: {
+        items: 1,
+        skip_validateItems: true,
+      },
+      740: {
+        items: 2,
+        skip_validateItems: true,
+      },
+      940: {
+        items: 3,
+        skip_validateItems: true,
+      },
+    },
+  };
   constructor(    public router: Router,
     ){
 
