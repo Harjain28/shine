0f50f66df06fd69614122ffbc9f9a1496a93d6c4
Harjain28 <harshit.appic@gmail.com>
2024-05-24 00:53:56 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,30 +3,27 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  Host_Url: "https://shine.creditenable.com/",
-  website_url: "https://shine.creditenable.com",
-  BASE_API_ENDPOINT: 'https://borrowerportal.creditenable.com/',
-  APPLICATION_BASE_POST_ELIGIBILITY_URL: 'https://borrowerportal.creditenable.com/api/',
-  BASE_API_ELIGIBILITY_URL_ENDPOINT: 'https://borrowerportal.creditenable.com/api/',
-  appInsights: {
-    instrumentationKey: 'b36c23f0-3b56-4cb1-aceb-0e1b1850a69c'
-  },
-    production: true,
-    enableGTM: true,
-
-
-
-
-  // Host_Url: "https://staging.creditenable.com/",
-  // website_url: "https://staging.creditenable.com",
-  // BASE_API_ENDPOINT: 'https://borrowerportal-staging.creditenable.com/',
-  // APPLICATION_BASE_POST_ELIGIBILITY_URL: 'https://borrowerportal-staging.creditenable.com/api/',
-  // BASE_API_ELIGIBILITY_URL_ENDPOINT: 'https://borrowerportal-staging.creditenable.com/api/',
-  // production: false,
+  // Host_Url: "https://shine.creditenable.com/",
+  // website_url: "https://shine.creditenable.com",
+  // BASE_API_ENDPOINT: 'https://borrowerportal.creditenable.com/',
+  // APPLICATION_BASE_POST_ELIGIBILITY_URL: 'https://borrowerportal.creditenable.com/api/',
+  // BASE_API_ELIGIBILITY_URL_ENDPOINT: 'https://borrowerportal.creditenable.com/api/',
   // appInsights: {
-  //   instrumentationKey: '9841128a-2c54-41a7-8dcb-67ca4d58bffb'
+  //   instrumentationKey: 'b36c23f0-3b56-4cb1-aceb-0e1b1850a69c'
   // },
-  // enableGTM: true,
+  //   production: true,
+  //   enableGTM: true,
+
+  Host_Url: "https://staging.creditenable.com/",
+  website_url: "https://staging.creditenable.com",
+  BASE_API_ENDPOINT: 'https://borrowerportal-staging.creditenable.com/',
+  APPLICATION_BASE_POST_ELIGIBILITY_URL: 'https://borrowerportal-staging.creditenable.com/api/',
+  BASE_API_ELIGIBILITY_URL_ENDPOINT: 'https://borrowerportal-staging.creditenable.com/api/',
+  production: false,
+  appInsights: {
+    instrumentationKey: '9841128a-2c54-41a7-8dcb-67ca4d58bffb'
+  },
+  enableGTM: true,
 
 };
 
